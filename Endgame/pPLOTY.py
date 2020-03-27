ad0e0e977bc627c9ca9fbae112ec25f1fb59d7b6
GenericPotato <37528053+GenericPotato@users.noreply.github.com>
2020-03-27 09:10:12 +0800

    WE NEED THE RATIO AND AREA STILL
    Please take note to DOWNLOAD THESE FILES AND PUT THEM INTO THE SAME FOLDER, python writes the files into the same folder directory. 
    
    Steps:
    1. Put them into the same folder
    2. Run it with ur desired number of circles and all that
    3. Call the files via pasting their file name into pPLOTY
    
    Sorry for the late upload! I was doing some small bugfixing because the code didnt seem to work on fresh computers at the time!

---
+++ Endgame/pPLOTY.py
@@ -0,0 +1,13 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import random as rdm
+import pickle as pick
+import matplotlib
+
+print(matplotlib.get_backend())
+
+test=pick.load(open("Succ Iter (92) Runs (14)","rb"))
+
+
+test.show()
